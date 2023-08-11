79c38e13a38c528494a88404b09ae4999a1af1e9
LilithAsh7 <lilly.ashbury@gmail.com>
2023-08-11 11:48:01 -0700

    Fixed coding error where I was using app.use instead of apiRouter.use, which was incorrect. Also changed require("./server/filename.js") since it didn't need the ./server part and just needed ./filename.js

--- project-4-boss-machine-start/server/api.js
+++ project-4-boss-machine-start/server/api.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-const minionsRouter = require('./server/minions');
-const ideasRouter = require('./server/ideas');
-const meetingsRouter = require('./server/meetings');
+const minionsRouter = require('./minions.js');
+const ideasRouter = require('./ideas.js');
+const meetingsRouter = require('./meetings.js');
 
-app.use('/api/minions', minionsRouter);
-app.use('/api/ideas', ideasRouter);
-app.use('/api/meetings', meetingsRouter);
+apiRouter.use('/api/minions', minionsRouter);
+apiRouter.use('/api/ideas', ideasRouter);
+apiRouter.use('/api/meetings', meetingsRouter);
 
 module.exports = apiRouter;
