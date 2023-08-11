3a2a534a0119fec16c36d6c30db88818ac9851ea
LilithAsh7 <lilly.ashbury@gmail.com>
2023-08-11 11:59:47 -0700

    Removed unused error handling code

--- project-4-boss-machine-start/server/meetings.js
+++ project-4-boss-machine-start/server/meetings.js
@@ -25,16 +25,4 @@ meetingsRouter.delete("/", (req, res, next) => {
     res.status(204).send()
 })
 
-/* 
-Simple error middleware.
-To use create a new error object with an error message string as a paramter like so:
-const errorVariableName = new Error('error message')
-Set err.status to whatever status code you want
-Then return next(errorVariableName)
-*/
-meetingsRouter.use((err, req, res, next) => {
-    const status = err.status || 500;
-    res.status(status).send(err.message);
-})
-
 module.exports = meetingsRouter;
