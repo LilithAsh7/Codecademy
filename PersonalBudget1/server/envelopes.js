8696973990e3fe37d3d38f7cefc179ad0ad01908
LilithAsh7 <lilly.ashbury@gmail.com>
2023-08-29 11:16:11 -0700

    Created envelope router and get request to send everything from the envelope database and post request to add a new envelope to the database.

---
+++ PersonalBudget1/server/envelopes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const envelopesRouter = express.Router();
+
+const {addNewCategory, getAllFromDatabase} = require("./db.js")
+
+envelopesRouter.get("/", (req, res, next) => {
+    const database = getAllFromDatabase()
+    res.send(database)
+})
+
+envelopesRouter.post("/", (req, res, next) => {
+    const newEnv = addNewCategory(req.body);
+    res.status(201).send(newEnv);
+})
+
+module.exports = envelopesRouter;
