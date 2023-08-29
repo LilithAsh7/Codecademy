d4fabe1d0cf7f7ea2ce0822bedf739b61c6907ba
LilithAsh7 <lilly.ashbury@gmail.com>
2023-08-29 11:14:43 -0700

    Set up simple budget envelope database and functions to work with database. Current functions get all from the database and add a new envelope.

---
+++ PersonalBudget1/server/db.js
@@ -0,0 +1,35 @@
+const envDb = [
+    {   
+        "category": "diningOut",
+        "amount": 0
+    },
+    {   
+        "category": "groceries",
+        "amount": 0
+    },
+    {   
+        "category": "fun",
+        "amount": 0
+    },
+    {   
+        "category": "gas",
+        "amount": 0
+    }
+]
+
+const getAllFromDatabase = () => {
+    return envDb
+}
+
+const addNewCategory = (newCategory) => {
+    toAdd = {
+                "category": newCategory,
+                "amount": 0
+            }
+    envDb.push(toAdd);
+}
+
+module.exports = {
+    addNewCategory,
+    getAllFromDatabase
+}
