5b29b10c89210cc8f526af89f22592eb853d8ff5
LilithAsh7 <lilly.ashbury@gmail.com>
2023-08-29 10:18:12 -0700

    Set up initial directory, simple endpoint for server, and initial git tracking.

---
+++ PersonalBudget1/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res, next) => {
+    res.send("Hello, World");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
